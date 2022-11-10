069b444d0c0218b113fc410dd9c5d52b441e10cc
Yael Chavez <you400@gmail.com>
2022-11-09 18:57:33 -0600

    Maps final

---
+++ VentaVehiculos/VentaVehiculos/Helpers/MapManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms.Maps;
+
+namespace VentaVehiculos.Helpers
+{
+    public class MapManager
+    {
+
+        public static Position GetXamPosition(Plugin.Geolocator.Abstractions.Position position)
+            => new Position(position.Latitude, position.Longitude);
+
+
+        public Map GetMap(bool currentPosition, Position position, Circle circle = null, List<Pin> pins = null)
+        {
+
+            var mapSpan = MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(30));
+
+            var map = new Map(mapSpan);
+
+            map.IsShowingUser = currentPosition;
+
+
+            if (circle != null)
+                map.MapElements.Add(circle);
+
+            if (pins != null)
+                pins.ForEach(x => map.Pins.Add(x));
+
+            return map;
+        }
+
+    }
+}
--- VentaVehiculos/VentaVehiculos/Views/NearCars.xaml.cs
+++ VentaVehiculos/VentaVehiculos/Views/NearCars.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Plugin.Geolocator;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO.Compression;
 using System.Threading.Tasks;
-
+using VentaVehiculos.Context;
+using VentaVehiculos.Helpers;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace VentaVehiculos.Views
@@ -12,9 +13,61 @@ namespace VentaVehiculos.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NearCars : ContentPage
     {
+        private readonly MapManager mapManager;
         public NearCars()
         {
             InitializeComponent();
+            mapManager = new MapManager();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await SetMapCars();
+        }
+
+        private async Task SetMapCars()
+        {
+            var locator = CrossGeolocator.Current;
+
+            locator.DesiredAccuracy = 100;
+
+            var position = await locator.GetPositionAsync();
+
+
+            List<Pin> pins = new List<Pin>();
+
+            var cars = await new CarsRepo().GetCars();
+
+            cars.ForEach(x =>
+            {
+
+                if (!(x.Lon == null || x.Lat == null))
+                {
+                    pins.Add(new Pin
+                    {
+
+                        Type = PinType.Generic,
+                        Label = x.Description,
+                        Address = x.Model,
+                        Position = new Position(x.Lat.Value, x.Lon.Value)
+
+                    });
+                }
+                
+            });
+
+            var circle = new Circle
+            {
+
+                Center = new Position(position.Latitude, position.Longitude),
+                Radius = new Distance(10000),
+                FillColor = Color.Blue
+            };
+
+
+            Content = mapManager.GetMap(true, new Position(position.Latitude, position.Longitude), circle, pins);
+
         }
     }
 }
